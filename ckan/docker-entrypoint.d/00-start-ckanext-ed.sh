5acd51b511d9edbcfb1e4116c3ad8853126d2128
higorspinto <higorspinto@gmail.com>
2021-04-29 13:36:37 -0300

    Update docker-compose environment.

---
+++ ckan/docker-entrypoint.d/00-start-ckanext-ed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Preparing ckanext-ed"
+
+paster --plugin=ckanext-ed init_survey_db
+paster --plugin=ckanext-ed ed create_ed_vocabularies
+paster --plugin=ckanext-ed ed create_ed_groups
+paster --plugin=ckanext-ed ed create_ed_organizations
+paster --plugin=ckanext-ed ed create_ed_data_explorers
