ad3b48d80d16db2e0f311ed1b3c3496ebac872b9
Tom Wood <tom.wood@civicactions.com>
2021-06-22 17:20:00 -0400

    Update requirements to "work" with PostgreSQL 12

--- ckan/update-requirements.sh
+++ ckan/update-requirements.sh
@@ -6,9 +6,9 @@
 cd requirements
 
 echo "Running poetry lock ..."
-poetry lock -vvv
+poetry lock -v
 echo "Running poetry export ..."
-poetry export -vvv --format requirements.txt --output ../ckan/requirements.txt --without-hashes
+poetry export -v --format requirements.txt --output ../ckan/requirements.txt --without-hashes
 poetry export --dev -vvv --format requirements.txt --output ../ckan/requirements-dev.txt --without-hashes
 
 # manually add ckanext-ed as a commented requirement for now
