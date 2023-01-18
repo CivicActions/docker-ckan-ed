3e8a9d38f6ba686fc855ef6f023545172f84ca1a
Michael Polidori <contact@michaelpolidori.com>
2023-01-17 20:04:41 -0500

    Add apache to Dockerfile and start with supervisor

--- ckan/docker-entrypoint.d/00-start-ckanext-ed.sh
+++ ckan/docker-entrypoint.d/00-start-ckanext-ed.sh
@@ -11,3 +11,4 @@ ckan -c ckan.ini edcli ed create_ed_organizations
 ckan -c ckan.ini edcli ed create_ed_data_explorers
 ckan -c ckan.ini edcli level-column
 ckan -c ckan.ini edcli omb-to-sources
+ckan -c ckan.ini edcli init-coordinators
